5fc479b5ad5995176b36bb457b9a9b3953e92a85
Navya Gaur <37734666+flash-sinx@users.noreply.github.com>
2020-09-27 15:53:08 +0530

    Add files via upload

---
+++ cfg.py
@@ -0,0 +1,12 @@
+config = {
+ "root url": "https://flinkhub.com",
+ "max_url": 10,
+ "sleep_time":5,
+ "time_diff": 1 
+}
+
+db_cfg = {
+    "host": 'localhost',
+    "port": 3000,
+    "db": 'CrawlerDB'
+    }
